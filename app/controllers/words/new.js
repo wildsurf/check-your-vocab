07ef1809482a66e874f50cefcba4c20abc56f292
Veronika Hillebrand <wildsurf.now@gmail.com>
2015-04-26 20:41:41 +0200

    adding an entry page for new words

---
+++ app/controllers/words/new.js
@@ -0,0 +1,57 @@
+import Ember from 'ember';
+import Session from 'check-your-vocab/utils/session.js';
+
+export default Ember.ObjectController.extend({
+
+  onSaveSuccess: function() {
+
+    this.transitionToRoute('words.index');
+
+  },
+
+  onSaveError: function(response) {
+
+    this.set('error', Ember.I18n.t(response.responseJSON.message));
+
+  },
+
+  saveLanguagePref: function() {
+
+    if (this.get('model.language1') && this.get('model.language2')) {
+
+      Session.languagePref = {
+        language1: this.get('model.language1'),
+        language2: this.get('model.language2')
+      };
+
+    }
+
+  },
+
+  modelObserver: function() {
+
+    if (!this.get('model')) {
+      return;
+    }
+
+    if (Session.languagePref) {
+
+      this.set('model.language1', Session.languagePref.language1);
+      this.set('model.language2', Session.languagePref.language2);
+
+    }
+
+  }.observes('model'),
+
+  actions: {
+
+    saveWord: function() {
+
+      this.saveLanguagePref();
+      this.get('model').save().then($.proxy(this.onSaveSuccess, this), $.proxy(this.onSaveError, this));
+
+    }
+
+  }
+
+});
